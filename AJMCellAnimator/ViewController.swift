926b1c9cfbaf1e4701a668a7fbf5177f3c70844e
Angel Morales <a.jesse3001@gmail.com>
2018-02-13 15:11:33 -0600

    implementando logica para voltear celdas

--- AJMCellAnimator/ViewController.swift
+++ AJMCellAnimator/ViewController.swift
@@ -42,7 +42,51 @@ extension ViewController : UICollectionViewDataSource, UICollectionViewDelegate 
         cell.titleLabel.text = dog
         cell.imageView.image = UIImage(named: dog)
         cell.imageView.contentMode = .scaleAspectFit
+        cell.imageView.alpha = 0
+        cell.titleLabel.alpha = 0
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath) as! DogCollectionViewCell
+        var transform = CATransform3DIdentity
+        transform.m34 = -0.002
+       
+        cell.layer.sublayerTransform = transform
+
+        let animation = UIViewPropertyAnimator(duration: 2.0, curve: .easeIn)
+        animation.addAnimations {
+            
+            UIView.animateKeyframes(
+                withDuration: 2.0,
+                delay: 0,
+                options: .calculationModeCubic,
+                animations: {
+                    
+                    UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1/3, animations: {
+                        cell.layer.transform = CATransform3DMakeRotation(CGFloat(M_PI_2), 0.0, 1.0, 0.0)
+                       
+                        
+                    })
+                    
+                    UIView.addKeyframe(withRelativeStartTime: 1/3, relativeDuration: 1/3, animations: {
+                        
+                        cell.layer.transform = CATransform3DMakeRotation(CGFloat(0), 0.0, 1.0, 0.0)
+                        cell.titleLabel.alpha = 1
+                        cell.imageView.alpha = 1
+                        
+                    })
+                    
+                    UIView.addKeyframe(withRelativeStartTime: 2/3, relativeDuration: 1/3, animations: {
+                        
+                    })
+            },
+                completion: { _ in
+            })
+        }
+       
+        animation.startAnimation()
+        
     }
 }
 
