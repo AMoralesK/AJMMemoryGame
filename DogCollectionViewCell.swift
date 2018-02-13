985d1832f35a7f4f5cde7963bbc5c6e691b104ec
Angel Morales <a.jesse3001@gmail.com>
2018-02-13 14:45:49 -0600

    agregando una clase collection view cell

---
+++ DogCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  DogCollectionViewCell.swift
+//  AJMCellAnimator
+//
+//  Created by Morales, Angel (MX - Mexico) on 13/02/18.
+//  Copyright © 2018 TheKairuz. All rights reserved.
+//
+
+import UIKit
+
+class DogCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+}
