60849d037d07adba34d2c869391cb69f56bc01ea
AngelMoralesK <a.jesse3001@gmail.com>
2018-10-17 14:38:35 -0500

    deleting the DogMemoryGame and adjusting the MemoryGame

--- AJMMemoryGame/MemoryGame.swift
+++ AJMMemoryGame/MemoryGame.swift
@@ -9,24 +9,60 @@
 import Foundation
 import UIKit
 
-protocol MemoryGame {
+class MemoryGame<T : Flippable>{
     
-    func revealCards(cardOne : Flippable, cardTwo : Flippable, completion:@escaping (_ status : Bool) ->())
-    func reverseCards(cardOne : Flippable, cardTwo : Flippable, completion:@escaping (_ status : Bool) ->())
-}
+    private var cardOne : T?
+    private var cardTwo : T?
+    private var lastTrackedCard : T?
 
-extension MemoryGame {
+    init(cardOne : T? = nil, cardTwo : T? = nil) { }
     
-    func revealCards(cardOne : Flippable, cardTwo : Flippable, completion:@escaping (_ status : Bool) ->()) {
+    func isValid(_ card :  T, collectionView : UICollectionView) -> Bool {
+        guard let lastTrackedCard = lastTrackedCard else { return true }
+        guard let indexPathOne =  collectionView.indexPath(for: card as! UICollectionViewCell), let indexPathTwo = collectionView.indexPath(for: lastTrackedCard as! UICollectionViewCell) else { return false }
+        return (indexPathOne == indexPathTwo) ? false : true
+    }
+    
+    
+    func prepare(_ card :  T, fromCollection collection: UICollectionView) {
         
-        var cardOne = cardOne
-        var cardTwo = cardTwo
+        if !isValid(card, collectionView: collection) {
+            lastTrackedCard = card
+            return
+        }
+        
+        if cardOne == nil {
+            cardOne = card
+        } else if cardTwo == nil {
+            cardTwo = card
+        }
+        
+        guard let cardOne = cardOne, let cardTwo = cardTwo else {
+            lastTrackedCard = card
+            return
+        }
+        
+        revealCards(temp1: cardOne, temp2: cardTwo, completion: { [unowned self](status) in
+            self.cardOne = nil
+            self.cardTwo = nil
+            self.lastTrackedCard = nil
+        })
+        
+       
+    }
+    
+    
+    func revealCards(temp1 : T, temp2 : T, completion:@escaping (_ status : Bool) ->()) {
+        
+    
+        let cardOne : UIView = temp1 as! UIView
+        let cardTwo : UIView  = temp2 as! UIView
 
         var transform = CATransform3DIdentity
         transform.m34 = -0.002
         
-        cardOne.flippableLayer.sublayerTransform = transform
-        cardTwo.flippableLayer.sublayerTransform = transform
+        cardOne.layer.sublayerTransform = transform
+        cardTwo.layer.sublayerTransform = transform
         
         let animation = UIViewPropertyAnimator(duration: 2.0, curve: .easeIn)
         animation.addAnimations {
@@ -38,20 +74,21 @@ extension MemoryGame {
                 animations: {
                     
                     UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1/3, animations: {
-                        cardOne.flippableLayer.transform = CATransform3DMakeRotation(CGFloat(Double.pi / 2), 0.0, 1.0, 0.0)
-                        cardTwo.flippableLayer.transform = CATransform3DMakeRotation(CGFloat(Double.pi / 2), 0.0, 1.0, 0.0)
+                        cardOne.layer.transform = CATransform3DMakeRotation(CGFloat(Double.pi / 2), 0.0, 1.0, 0.0)
+                        cardTwo.layer.transform = CATransform3DMakeRotation(CGFloat(Double.pi / 2), 0.0, 1.0, 0.0)
                         
                     })
                     
                     UIView.addKeyframe(withRelativeStartTime: 1/3, relativeDuration: 1/3, animations: {
                         
-                        cardOne.flippableLayer.transform = CATransform3DMakeRotation(CGFloat(0), 0.0, 1.0, 0.0)
-                        cardOne.reveal()
+                        cardOne.layer.transform = CATransform3DMakeRotation(CGFloat(0), 0.0, 1.0, 0.0)
+                        
+                        temp1.reveal()
                         //cardOne.titleLabel.alpha = 1
                         //cardOne.imageView.alpha = 1
                         
-                        cardTwo.flippableLayer.transform = CATransform3DMakeRotation(CGFloat(0), 0.0, 1.0, 0.0)
-                        cardTwo.reveal()
+                        cardTwo.layer.transform = CATransform3DMakeRotation(CGFloat(0), 0.0, 1.0, 0.0)
+                        temp2.reveal()
                        // cardTwo.titleLabel.alpha = 1
                        // cardTwo.imageView.alpha = 1
                         
@@ -67,8 +104,8 @@ extension MemoryGame {
         
         animation.addCompletion { (position) in
             print(position)
-            if !cardOne.matches(cell: cardTwo) {
-                self.reverseCards(cardOne: cardOne, cardTwo: cardTwo, completion: { (status) in
+            if !temp1.matches(elem: temp2) {
+                self.reverseCards(temp1: temp1, temp2: temp2, completion: { (status) in
                     completion(true)
                 })
             } else {
@@ -76,8 +113,8 @@ extension MemoryGame {
                 cardOne.isUserInteractionEnabled = false
                 cardTwo.isUserInteractionEnabled = false
                 
-                cardOne.flippableAlpha = 0.5
-                cardTwo.flippableAlpha = 0.5
+                cardOne.alpha = 0.5
+                cardTwo.alpha = 0.5
                 completion(true)
             }
             
@@ -86,7 +123,10 @@ extension MemoryGame {
         animation.startAnimation()
     }
     
-    func reverseCards(cardOne : Flippable, cardTwo : Flippable, completion:@escaping (_ status : Bool) ->() ) {
+    func reverseCards(temp1 : T, temp2 : T, completion:@escaping (_ status : Bool) ->() ) {
+        
+        let cardOne : UIView = temp1 as! UIView
+        let cardTwo : UIView  = temp2 as! UIView
         
         let animation = UIViewPropertyAnimator(duration: 2.0, curve: .easeIn)
         animation.addAnimations {
@@ -98,13 +138,13 @@ extension MemoryGame {
                 animations: {
                     
                     UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1/2, animations: {
-                        cardOne.flippableLayer.transform = CATransform3DMakeRotation(-CGFloat(Double.pi / 2), 0.0, 1.0, 0.0)
-                        cardOne.unreveal()
+                        cardOne.layer.transform = CATransform3DMakeRotation(-CGFloat(Double.pi / 2), 0.0, 1.0, 0.0)
+                        temp1.unreveal()
                        // cardOne.titleLabel.alpha = 0
                        // cardOne.imageView.alpha = 0
                         
-                        cardTwo.flippableLayer.transform = CATransform3DMakeRotation(-CGFloat(Double.pi / 2), 0.0, 1.0, 0.0)
-                        cardTwo.unreveal()
+                        cardTwo.layer.transform = CATransform3DMakeRotation(-CGFloat(Double.pi / 2), 0.0, 1.0, 0.0)
+                        temp2.unreveal()
                         //  cardTwo.titleLabel.alpha = 0
                       //  cardTwo.imageView.alpha = 0
                         
@@ -112,13 +152,13 @@ extension MemoryGame {
                     })
                     
                     UIView.addKeyframe(withRelativeStartTime: 1/2, relativeDuration: 1/2, animations: {
-                        cardOne.flippableLayer.transform = CATransform3DMakeRotation(0, 0.0, 1.0, 0.0)
-                        cardOne.unreveal()
+                        cardOne.layer.transform = CATransform3DMakeRotation(0, 0.0, 1.0, 0.0)
+                        temp1.unreveal()
                       //  cardOne.titleLabel.alpha = 0
                       //  cardOne.imageView.alpha = 0
                         
-                        cardTwo.flippableLayer.transform = CATransform3DMakeRotation(0, 0.0, 1.0, 0.0)
-                        cardTwo.unreveal()
+                        cardTwo.layer.transform = CATransform3DMakeRotation(0, 0.0, 1.0, 0.0)
+                        temp2.unreveal()
                      //   cardTwo.titleLabel.alpha = 0
                      //   cardTwo.imageView.alpha = 0
                         
@@ -140,11 +180,15 @@ extension MemoryGame {
 }
 
 
-protocol Flippable {
+protocol Flippable   {
     func reveal()
     func unreveal()
-    func matches(cell : Flippable) -> Bool
-    var flippableLayer : CALayer { set get }
-    var flippableAlpha : CGFloat { set get }
-    var isUserInteractionEnabled : Bool { set get }
+    func matches(elem : Flippable) -> Bool
+
+   // func matches(cell : Flippable) -> Bool
+ //   var flippableLayer : CALayer { set get }
+ //   var isUserInteractionEnabled : Bool { set get }
 }
+
+
+
